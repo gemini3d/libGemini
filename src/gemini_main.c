b4503d83cd78287a739dd7bf6002bb72edb258af
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-01 12:11:43 -0400

    main struct API

--- src/gemini_main.c
+++ src/gemini_main.c
@@ -10,31 +10,57 @@
 
 int main(int argc, char **argv) {
 
+struct params s;
+
 int ierr = MPI_Init(&argc, &argv);
 
 if (argc < 2) {
-  perror("please give simulation output directory e.g. ~/data/my_sim");
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3D: please give simulation output directory e.g. ~/data/my_sim\n");
   return 1;
 }
 
-int Lpath_max = 10000;
 int L = strlen(argv[1]);
-if(L > Lpath_max) {
-  // arbitrary maximum path length--10000 is not referred to anywhere else in the program.
-  fprintf(stderr, "Gemini3D simulation output directory: path length > %d", Lpath_max);
+if(L > LMAX) {
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3D simulation output directory: path length > %d\n", LMAX);
   return 1;
 }
 L++; // for null terminator
 
-char *out_dir = (char *)malloc(L);
-if (out_dir != NULL) {
-  strcpy(out_dir, argv[1]);
+if(strcmp(argv[1], "-") == 0) {
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3d: first argument must be output directory\n");
+  return 1;
 }
 
-int lid2in = -1, lid3in = -1;
-bool fortran_cli = false;
+strcpy(s.out_dir, argv[1]);
 
-gemini_main(out_dir, &L, &lid2in, &lid3in, &fortran_cli);
+s.fortran_cli = false;
+s.debug = false;
+s.dryrun = false;
+int lid2in = -1, lid3in = -1;
+
+for (int i = 2; i < argc; i++) {
+  if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "-debug") == 0) s.debug = true;
+  if (strcmp(argv[i], "-dryrun") == 0) s.dryrun = true;
+  if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-help") == 0) {
+    MPI_Finalize();
+    help_gemini_bin();
+    return 0;
+  }
+  if (strcmp(argv[i], "-manual_grid") == 0) {
+    if (argc < i+1) {
+      MPI_Finalize();
+      fprintf(stderr, "-manual_grid lid2in lid3in\n");
+      return 1;
+    }
+    lid2in = atoi(argv[i]);
+    lid3in = atoi(argv[i+1]);
+  }
+}
+
+gemini_main(s, &lid2in, &lid3in);
 
 ierr = MPI_Finalize();
 
