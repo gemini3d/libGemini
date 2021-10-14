06af98a8e85266768f98cfc88f424ff35505abaf
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-13 21:29:17 -0400

    add cmake options.cmake

---
+++ cmake/options.cmake
@@ -0,0 +1,20 @@
+option(dev "development mode")
+
+
+if(dev)
+else()
+  set_directory_properties(PROPERTIES EP_UPDATE_DISCONNECTED true)
+endif()
+
+
+# --- auto-ignore build directory
+if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
+  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
+endif()
+
+# --- default install directory under build/local
+# users can specify like "cmake -B build --install-prefix=~/mydir"
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
+endif()
