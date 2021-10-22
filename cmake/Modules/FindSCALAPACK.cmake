74be8a7c6e6fdb472c224a9a4b8d7e49cf4fcb4f
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-22 12:32:41 -0400

    FindLAPACK: Hint LAPACK95_ROOT

--- cmake/Modules/FindSCALAPACK.cmake
+++ cmake/Modules/FindSCALAPACK.cmake
@@ -62,7 +62,10 @@ if(NOT Fortran IN_LIST enabled_langs)
 endif()
 
 find_package(MPI COMPONENTS C Fortran)
-find_package(LAPACK)
+if(NOT LAPACK_FOUND)
+  # otherwise can cause 32-bit lapack when 64-bit wanted
+  find_package(LAPACK)
+endif()
 if(NOT (MPI_Fortran_FOUND AND LAPACK_FOUND))
   return()
 endif()
@@ -173,13 +176,22 @@ else()
 
   find_package(PkgConfig)
 
-  pkg_search_module(pc_scalapack scalapack-openmpi scalapack-mpich scalapack)
+  pkg_search_module(pc_scalapack scalapack scalapack-openmpi scalapack-mpich)
 
   find_library(SCALAPACK_LIBRARY
     NAMES scalapack scalapack-openmpi scalapack-mpich
     NAMES_PER_DIR
     HINTS ${pc_scalapack_LIBRARY_DIRS} ${pc_scalapack_LIBDIR}
     PATH_SUFFIXES openmpi/lib mpich/lib
+  )
+
+  # some systems have libblacs as a separate file, instead of being subsumed in libscalapack.
+  cmake_path(GET SCALAPACK_LIBRARY PARENT_PATH BLACS_ROOT)
+
+  find_library(BLACS_LIBRARY
+    NAMES blacs
+    NO_DEFAULT_PATH
+    HINTS ${BLACS_ROOT}
   )
 
 endif()
@@ -200,12 +212,15 @@ find_package_handle_standard_args(SCALAPACK
 if(SCALAPACK_FOUND)
 # need if _FOUND guard to allow project to autobuild; can't overwrite imported target even if bad
 set(SCALAPACK_LIBRARIES ${SCALAPACK_LIBRARY})
+if(BLACS_LIBRARY)
+  list(APPEND SCALAPACK_LIBRARIES ${BLACS_LIBRARY})
+endif()
 set(SCALAPACK_INCLUDE_DIRS ${SCALAPACK_INCLUDE_DIR})
 
 if(NOT TARGET SCALAPACK::SCALAPACK)
   add_library(SCALAPACK::SCALAPACK INTERFACE IMPORTED)
   set_target_properties(SCALAPACK::SCALAPACK PROPERTIES
-                        INTERFACE_LINK_LIBRARIES "${SCALAPACK_LIBRARY}"
+                        INTERFACE_LINK_LIBRARIES "${SCALAPACK_LIBRARIES}"
                         INTERFACE_INCLUDE_DIRECTORIES "${SCALAPACK_INCLUDE_DIR}"
                       )
 endif()
